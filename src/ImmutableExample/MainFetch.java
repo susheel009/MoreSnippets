8418ecec2c09f29ffd861bd699522e5ac35e2f40
susheel <susheel009@gmail.com>
2024-06-11 12:44:30 -0400

    Revert "First commit of this project"
    This reverts commit 74678a8344fbdb502aa5f3a9a82836679340f68f.

--- src/ImmutableExample/MainFetch.java
+++
@@ -1,8 +0,0 @@
-package ImmutableExample;
-
-public class MainFetch {
-    public static void main(String[] args) {
-        DetailsFetch df = new DetailsFetch("Susheel", 7001,"susheel009@gmail.com",123436 );
-        System.out.println(df);
-    }
-}
